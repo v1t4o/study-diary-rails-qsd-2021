e68caabb6cc3abbda5a8e960154e8a396e797695
Victor Oliveira <vitor.oliveira.marinho@gmail.com>
2021-12-03 12:28:21 -0300

    model+controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,9 @@ class HomeController < ApplicationController
   def index
     @title = 'Ruby Diary'
     @study_items = []
-    @study_items << { name: 'HEREDOC', category: 'Ruby'}
-    @study_items << { name: 'Guard Clause', category: 'Ruby'}
+    @study_items << StudyItem.new(name: 'HEREDOC', category: 'Ruby')
+    @study_items << StudyItem.new(name: 'Guard Clause', category: 'Ruby')
+    @study_items << StudyItem.new(name: 'Guard Clause', category: 'Ruby')
   end
 
 end
