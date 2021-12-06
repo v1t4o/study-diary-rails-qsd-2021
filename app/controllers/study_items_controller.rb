38ec399fa493366835b91eda79db0cee9cf010e3
Victor Oliveira <vitor.oliveira.marinho@gmail.com>
2021-12-06 10:49:28 -0300

    tweaks and new navigation links

--- app/controllers/study_items_controller.rb
+++ app/controllers/study_items_controller.rb
@@ -1,5 +1,5 @@
 class StudyItemsController < ApplicationController
-  
+
   def index
     @study_items = StudyItem.all
   end
@@ -19,6 +19,25 @@ class StudyItemsController < ApplicationController
 
   def show
     @study_item = StudyItem.find(params[:id])
+  end
+
+  def edit
+    @study_item = StudyItem.find(params[:id])
+  end
+
+  def update
+    @study_item = StudyItem.find(params[:id])
+    if @study_item.update(study_item_params)
+      return redirect_to root_path 
+    end
+    
+    render :edit
+  end
+
+  def destroy
+    @study_item = StudyItem.find(params[:id])
+    @study_item.delete(params[:id])
+    return redirect_to root_path 
   end
 
   private
